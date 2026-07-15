a71d9e5640e1551b414c3c785e1216db17f6967a
andriibats <Andrii.Bats@skai.io>
2026-07-16 00:18:40 +0200

    [milestone] Task 5: add agentry-cli module (Spring Shell)
    - Spring Shell starter dependency
    - Package structure for CLI commands

--- agentry-cli/build.gradle.kts
+++ agentry-cli/build.gradle.kts
@@ -1,1 +1,10 @@
-dependencies {}
+plugins {
+    alias(libs.plugins.spring.boot) apply false
+}
+
+dependencies {
+    implementation(project(":agentry-core"))
+    implementation(libs.spring.shell.starter)
+
+    testImplementation(libs.spring.boot.starter.test)
+}
